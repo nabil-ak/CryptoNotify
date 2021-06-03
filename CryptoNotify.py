72d3af8a1da44d9134b20dafda7abd3838a19485
Nabil <nabil.akir@outlook.de>
2021-06-03 16:46:52 +0200

    add README and some minor changes

--- CryptoNotify.py
+++ CryptoNotify.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from discord.ext import tasks
 settings = json.load(open("settings.json"))
 class Bot(discord.Client):
-    lastmessage = None
+
     async def on_ready(self):
         print("Connected")
         self.currentWallet.start()
@@ -19,25 +19,31 @@ class Bot(discord.Client):
     @tasks.loop(hours=1)
     async def currentWallet(self):
         message = ""
-        kanal = self.get_channel(849781234318049331)
-        gesamt = 0
-        lastGesamt = 0
+        kanal = self.get_channel(settings["channelID"])
+        total = 0
+        lastTotal = 0
+        message += "------------------------------------------------------------------\n"
         for price in settings["LastPrice"]:
             response = requests.get(f"https://api.coinbase.com/v2/prices/{price}-EUR/spot")
             response = json.loads(response.text)
+
             amount = settings["wallet"][price]
-            currentPrice = round(float(response["data"]["amount"])*amount,2)
-            lastPrice = round(settings["LastPrice"][price]*amount,2)
-            message += f"{self.getEmoji(currentPrice, lastPrice)} **{price}**: {lastPrice} ---> {currentPrice}\n"
+            lastExchangerate = settings["LastPrice"][price]
+            currentExchangerate = float(response["data"]["amount"])
+            currentPrice = round(currentExchangerate*amount,2)
+            lastPrice = round(lastExchangerate*amount,2)
+
+            message += f"{self.getEmoji(currentPrice, lastPrice)} **{price}**: {lastPrice}€ ---> {currentPrice}€ | {lastExchangerate}€ ---> {currentExchangerate}€ | **{self.getPercent(lastPrice,currentPrice)}%**\n"
             settings["LastPrice"][price] = float(response["data"]["amount"])
-            gesamt+=currentPrice
-            lastGesamt+=lastPrice
-        message += "------------------------------------------------\n"
-        message += f"{self.getEmoji(gesamt, lastGesamt)} **Total**: {round(lastGesamt,2)} ---> {round(gesamt,2)}\n"
+            total+=currentPrice
+            lastTotal+=lastPrice
+        message += "------------------------------------------------------------------\n"
+        message += f"{self.getEmoji(total, lastTotal)} **Total**: {round(lastTotal,2)}€ ---> {round(total,2)}€ | **{self.getPercent(lastTotal,total)}%**\n"
         json.dump(settings, open("settings.json", "w"))
-        if self.lastmessage != None:
-            await self.lastmessage.delete()
-        self.lastmessage = await kanal.send(message)
+        messages = await kanal.history(limit=200).flatten()
+        await kanal.delete_messages(messages)
+        await kanal.send(message)
+
     def getEmoji(self, currentPrice, lastPrice):
         if lastPrice < currentPrice:
             return "📈"
@@ -45,6 +51,8 @@ class Bot(discord.Client):
             return "📉"
         else:
             return "=="
+    def getPercent(self, lastprice, newprice):
+        return round((100/lastprice)*(newprice-lastprice),2)
 
 client = Bot()
 client.run(settings["token"])
--- README.md
+++ README.md
@@ -1,8 +1,38 @@
+![Icon](icon.png)
 # CryptoNotify
 
 Discord Bot who notify you every hour with the newest Crypto prices.
 
+## Installation
 
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the requirement frameworks.
+
+```bash
+pip install -r requirements.txt
+```
+## Settings
+1. Change the **channelID** and the **token**.
+2. Rename **settings.example.json** to **settings.json**
+3. Add Cryptocurrencies to your **Wallet** and **LastPrice** (Ignore the price. The bot sets the price itself)
+
+**Be aware just use Cryptocurrencies from [Coinbase](https://www.coinbase.com/price)**
+
+```json
+{
+    "channelID": 92349347234436,
+    "token": "uwertgwreugt23402340",
+    "wallet": {
+        "BTC": 0.00683409,
+        "ETH": 0.02297584
+    },
+    "LastPrice": {
+        "BTC": 31657.49,
+        "ETH": 2284.24
+    }
+}
+```
+## Usage
+Just run ```CryptoNotify.py```
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
